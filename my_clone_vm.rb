cc228594faf6a67caec73401b4e329c587b830a7
Pablo Halamaj <phalamaj@semperti-n-008.semperti.local>
2014-10-16 10:50:23 -0300

    Este va a ser el codigo de mi method

---
+++ my_clone_vm.rb
@@ -0,0 +1,8 @@
+###################################
+#
+# EVM Automate Method: my_clone_vm
+#
+# Notes:This method is used to clone a VM with the exact same parameters as the origin
+#	but the resulting VM will have the "cloned" Tag.
+#
+###################################
