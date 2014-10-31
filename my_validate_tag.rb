d55d4c17ae580db38d84ac4c23104daa960cbcdf
Pablo Halamaj <pablo.halamaj@gmail.com>
2014-10-30 23:37:17 -0300

    funciona el detector de Tag - Hardcodeado

--- my_validate_tag.rb
+++ my_validate_tag.rb
@@ -4,47 +4,62 @@ begin
   @method = 'my_validate_tag'
   $evm.log("info", "===== EVM Automate Method: <#{@method}> Started")
 
+  # Turn of debugging
+  @debug = true
+
   # Log the inbound object
-  $evm.log("info", "<#{@method}>--===========================================")
+  $evm.log("info", "===========================================")
   process = $evm.object("process")
-  $evm.log("info", "<#{@method}>--Listing Process Attributes:")
-  process.attributes.sort.each { |k, v| $evm.log("info", "\t#{k}: #{v}")}
-  $evm.log("info", "<#{@method}>--===========================================")
+  $evm.log("info", "Listing Process Attributes:")
+  process.attributes.sort.each { |k, v| $evm.log("info", "\t#{k}: #{v}")} if @debug
+  $evm.log("info", "===========================================")
 
-  # start working
-  current = $evm.object
-  $evm.log("info", "<#{@method}>--Got current object")
+  vm = $evm.root['vm']
+  $evm.log("info", "Got VM from root object")
+  $evm.log("info","Inspecting vm: #{vm.inspect}") if @debug
 
-  vm        = current["vm"]
-  $evm.log("info", "<#{@method}>--Got VM #{vm} from current object")
   ems       = vm.ext_management_system
-  $evm.log("info", "Got EMS from VM")
-  host      = vm.host
-  $evm.log("info", "Got Host from VM")
+  $evm.log("info", "Got EMS <#{ems.name}> from VM <#{vm.name}>")
+  $evm.log("info","Inspecting ems: #{ems.inspect}") if @debug
 
-  #Find the tags of the vm  with the least number of vms
-  tagged_vm	= nil
-  tag		= "cloned"
-  tagged_vm     = $evm.vmdb(:vm).tagged_with(:Custom_Tags,"cloned")
-  $evm.log("info", "<#{@method}>--Got #{tagged_vm} Hosts tagged with #{tag}")
+  host      = vm.host
+  $evm.log("info", "Got Host <#{host.name}> from VM <#{vm.name}>")
+  $evm.log("info","Inspecting host: #{host.inspect}") if @debug
+
+
+  #Find the tags of the vm 
+  unless vm.tags.nil?
+  tag           = "cloned"
+  vm.tags.sort.each {|tag_element| tag_text = tag_element.split('/');
+    $evm.log("info", "#{@method} - VM:<#{vm.name}> Category:<#{tag_text.first.inspect}> Tag:<#{tag_text.last.inspect}>")
+    first_tag = tag_text.first
+    second_tag = tag_text.second
+    if first_tag == "cloned"
+      $evm.log("error", "<#{@method}>: VM Tagged with <#{tag_text.first.inspect}> y <#{tag_text.second.inspect}>")
+        exit MIQ_STOP
+    else
+      $evm.log("")
+    end }
+   end
+
 
   if tagged_vm == nil
-	  #
-	  # Exit method
-	  #
-	  $evm.log("info", "<#{@method}>--===== #{vm.name} tagged with tag #{tag} =========")
-	  $evm.log("info", "===== EVM Automate Method: <#{@method}> Ended")
-	  exit MIQ_OK
+          #
+          # Exit method
+          #
+          $evm.log("info", "<#{@method}>--===== #{vm.name} tagged with tag #{tag} =========")
+          $evm.log("info", "===== EVM Automate Method: <#{@method}> Ended")
+          exit MIQ_OK
   else
   #
   # Set Ruby rescue behavior
   #
-	$evm.log("info", "<#{@method}>--===== #{vm.name} not tagged with tag #{tag} =========")
+        $evm.log("info", "<#{@method}>--===== #{vm.name} not tagged with tag #{tag} =========")
         $evm.log("info", "===== EVM Automate Method: <#{@method}> Ended")
-	exit MIQ_STOP
+        exit MIQ_STOP
   end
 
-	rescue => err
+        rescue => err
         $evm.log("error", "<#{@method}>: [#{err}]\n#{err.backtrace.join("\n")}")
         exit MIQ_STOP
 end
